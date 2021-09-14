565fe9a626c7bc54d6ecc15538b280cfd08e9bbd
Dan Jones <dcjones.work@gmail.com>
2021-09-14 22:47:08 +0100

    Add single TS plot function (all depths)

--- src/plot_tools.py
+++ src/plot_tools.py
@@ -758,6 +758,67 @@ def plot_TS_single_lev(ploc, df, n_comp, descrip='', plev=0, PTrange=(-2, 27.0),
     plt.close()
 
 #####################################################################
+# T-S plot for all pressure levels
+#####################################################################
+def plot_TS_all_lev(ploc, df, n_comp, descrip='', PTrange=(-2, 27.0),
+                    SPrange=(33.5, 37.5), lon = -20, lat = -65, rr = 0.33):
+
+    # make 1D by stacking and resetting index
+    df1D = df.stack(z=('profile','depth')).reset_index('z')
+
+    # define colormap
+    colormap = plt.get_cmap('tab10', n_comp)
+
+    # grid
+    pt_grid = np.linspace(PTrange[0],PTrange[1],100)
+    sp_grid = np.linspace(SPrange[0],SPrange[1],100)
+    p = df.depth.values[0]
+    lon = -20
+    lat = -65
+
+    sa_grid = gsw.SA_from_SP(sp_grid, p, lon, lat)
+    ct_grid = gsw.CT_from_pt(sa_grid, pt_grid)
+    ctg,sag = np.meshgrid(ct_grid,sa_grid)
+    sig0_grid = gsw.density.sigma0(sag, ctg)
+
+    # extract values as new DataArrays
+    T = df1D.prof_CT.values
+    S = df1D.prof_SA.values
+    clabels = df1D.label.values
+
+    # size of random sample (all profiles by now)
+    random_sample_size = int(np.ceil(rr*df1D.profile.size))
+
+    # random sample for plotting
+    rows_id = random.sample(range(0,clabels.size-1), random_sample_size)
+    T_random_sample = T[rows_id]
+    S_random_sample = S[rows_id]
+    clabels_random_sample = clabels[rows_id]
+
+    #colormap with Historical data
+    plt.figure(figsize=(13, 13))
+    CL = plt.contour(ctg, sag, sig0_grid, colors='black', zorder=1)
+    plt.clabel(CL, fontsize=24, inline=False, fmt='%.1f')
+    SC = plt.scatter(T_random_sample,
+                     S_random_sample,
+                     c = clabels_random_sample,
+                     marker='o',
+                     cmap= colormap,
+                     s=8.0,
+                     zorder=2,
+                     )
+    plt.colorbar(SC)
+    plt.xlabel('Conservative temperature [$^\circ$C]', fontsize=20)
+    plt.ylabel('Absolute salinity [psu]', fontsize=20)
+    plt.xlim(PTrange)
+    plt.ylim(SPrange)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    #plt.title('T-S diagram at '+ str(p) + ' dbar', fontsize=22)
+    plt.savefig(ploc + 'TS_all_levs' + descrip + '.png', bbox_inches='tight')
+    plt.close()
+
+#####################################################################
 # T-S plot for a multiple pressure levels (one for each class)
 #####################################################################
 def plot_TS_multi_lev(ploc, df, n_comp, descrip='', plev=0, PTrange=(-2, 27.0),
@@ -824,6 +885,6 @@ def plot_TS_multi_lev(ploc, df, n_comp, descrip='', plev=0, PTrange=(-2, 27.0),
         plt.ylim(SPrange)
         plt.xticks(fontsize=18)
         plt.yticks(fontsize=18)
-        plt.title('T-S diagram at '+ str(p) + ' dbar', fontsize=22)
+        plt.title('Class ' + str(nclass) , fontsize=22)
         plt.savefig(ploc + 'TS_multilev_class_' + str(nclass) + 'K' + descrip + '.png', bbox_inches='tight')
         plt.close()
