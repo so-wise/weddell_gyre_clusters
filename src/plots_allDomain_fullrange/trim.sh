127403b047b8efacd2b0bd115b5acfc24ae0c1ac
Dan Jones <dcjones.work@gmail.com>
2021-10-22 13:11:06 +0100

    Reduce size

--- src/plots_allDomain_fullrange/trim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-convert pca_scatter_nolabels_view1.png -trim pca_scatter_nolabels_view1.png
-convert pca_scatter_nolabels_view2.png -trim pca_scatter_nolabels_view2.png
-convert pca_scatter_nolabels_view3.png -trim pca_scatter_nolabels_view3.png
-convert pca_scatter_labelled_view1.png -trim pca_scatter_labelled_view1.png
-convert pca_scatter_labelled_view2.png -trim pca_scatter_labelled_view2.png
-convert pca_scatter_labelled_view3.png -trim pca_scatter_labelled_view3.png
