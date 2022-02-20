da37bac6bc85d8286735a0548f2b5e42fab9100d
Dan Jones <dcjones.work@gmail.com>
2022-02-20 00:18:59 +0000

    Added a dynamic height function

--- src/density.py
+++ src/density.py
@@ -20,6 +20,7 @@ def calc_density(profiles):
     lon = profiles.lon
     lat = profiles.lat
 
+    # apply functions
     sa = xr.apply_ufunc(gsw.SA_from_SP, sp, p, lon, lat, dask='parallelized', output_dtypes=[sp.dtype])
     ct = xr.apply_ufunc(gsw.CT_from_pt, sa, pt, dask='parallelized', output_dtypes=[sa.dtype])
     sig0 = xr.apply_ufunc(gsw.density.sigma0, sa, ct, dask='parallelized', output_dtypes=[sa.dtype])
@@ -31,6 +32,34 @@ def calc_density(profiles):
 
     # return it
     return profiles
+
+#####################################################################
+# Calculate dynamic height anomaly, N2,
+#####################################################################
+def calc_geostropic_currents(profiles):
+
+    # display
+    print('density.calc_geostrophic_currents')
+
+    # extract a few variables
+    sa = profiles.prof_SA.T
+    ct = profiles.prof_CT.T
+    p = profiles.depth
+    lon = profiles.lon
+    lat = profiles.lat
+
+    # apply function : get dynamic height
+    dynamic_height = xr.apply_ufunc(gsw.geostrophy.geo_strf_dyn_height,
+        sa, ct, p, dask='parallelized', output_dtypes=[sa.dtype])
+
+    # geostrophic velocity
+    #geo_vel, mid_lon, mid_lat = xr.apply_ufunc(gsw.geostrophy.geostrophic_velocity,
+    #    dynamic_height, lon, lat, dask='parallelized', output_dtypes=[sa.dtype])
+
+    #Nsquared = xr.apply_ufunc(gsw.stability.Nsquared, sa, ct, p,
+    #    dask='parallelized', output_dtypes=[sa.dtype])
+
+    return dynamic_height 
 
 #####################################################################
 # Scalar density value
